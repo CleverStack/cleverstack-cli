1f5a095195223006dc7ba68d517c8b05ba87238d
Daniel Durante <me@danieldurante.com>
2014-02-11 21:18:21 -0500

    chore(clean): Cleans up generate/generate tests
    * Cleans lib/generator.js
    * Cleans tests/generate-backend.test.js
    * Adds a new gitignore rule to ignore everything but .gitkeep in
    * tests/assets

--- tests/generate-backend.test.js
+++ tests/generate-backend.test.js
@@ -24,13 +24,8 @@ describe( 'Generate backend seed', function ( ) {
         expect( stderr ).to.equal( '' );
         expect( stdout ).to.not.match( /already exists within/ );
 
-        expect( fs.existsSync( path.join( assetPath, 'my-new-project', 'backend', 'modules', 'Testing2', 'config' ) ) ).to.be.false;
+        expect( fs.readdirSync( path.join( assetPath, 'my-new-project', 'backend', 'modules', 'Testing2') ).length ).to.equal( 1 );
         expect( fs.existsSync( path.join( assetPath, 'my-new-project', 'backend', 'modules', 'Testing2', 'controllers', 'Testing2Controller.js' ) ) ).to.be.true;
-        expect( fs.existsSync( path.join( assetPath, 'my-new-project', 'backend', 'modules', 'Testing2', 'models' ) ) ).to.be.false;
-        expect( fs.existsSync( path.join( assetPath, 'my-new-project', 'backend', 'modules', 'Testing2', 'schema' ) ) ).to.be.false;
-        expect( fs.existsSync( path.join( assetPath, 'my-new-project', 'backend', 'modules', 'Testing2', 'services' ) ) ).to.be.false;
-        expect( fs.existsSync( path.join( assetPath, 'my-new-project', 'backend', 'modules', 'Testing2', 'tasks' ) ) ).to.be.false;
-        expect( fs.existsSync( path.join( assetPath, 'my-new-project', 'backend', 'modules', 'Testing2', 'tests' ) ) ).to.be.false;
 
         var controller = fs.readFileSync( path.join( assetPath, 'my-new-project', 'backend', 'modules', 'Testing2', 'controllers', 'Testing2Controller.js' ) );
         expect( controller ).to.match( /module\.exports = function\( Testing2Service \) \{/ );
@@ -64,14 +59,9 @@ describe( 'Generate backend seed', function ( ) {
         expect( stderr ).to.equal( '' );
         expect( stdout ).to.not.match( /already exists within/ );
 
-        expect( fs.existsSync( path.join( assetPath, 'my-new-project', 'backend', 'modules', 'Testing2', 'config' ) ) ).to.be.false;
-        expect( fs.existsSync( path.join( assetPath, 'my-new-project', 'backend', 'modules', 'Testing2', 'controllers' ) ) ).to.be.false;
+        expect( fs.readdirSync( path.join( assetPath, 'my-new-project', 'backend', 'modules', 'Testing2') ).length ).to.equal( 1 );
         expect( fs.existsSync( path.join( assetPath, 'my-new-project', 'backend', 'modules', 'Testing2', 'models', 'odm', 'Testing2Model.js' ) ) ).to.be.true;
         expect( fs.existsSync( path.join( assetPath, 'my-new-project', 'backend', 'modules', 'Testing2', 'models', 'odm', 'Testing2Model.js' ) ) ).to.be.true;
-        expect( fs.existsSync( path.join( assetPath, 'my-new-project', 'backend', 'modules', 'Testing2', 'schema' ) ) ).to.be.false;
-        expect( fs.existsSync( path.join( assetPath, 'my-new-project', 'backend', 'modules', 'Testing2', 'services' ) ) ).to.be.false;
-        expect( fs.existsSync( path.join( assetPath, 'my-new-project', 'backend', 'modules', 'Testing2', 'tasks' ) ) ).to.be.false;
-        expect( fs.existsSync( path.join( assetPath, 'my-new-project', 'backend', 'modules', 'Testing2', 'tests' ) ) ).to.be.false;
 
         var odmModel = fs.readFileSync( path.join( assetPath, 'my-new-project', 'backend', 'modules', 'Testing2', 'models', 'odm', 'Testing2Model.js' ) );
         expect( odmModel ).to.match( /return mongoose\.model\('Testing2', ModelSchema\);/ );
@@ -106,13 +96,8 @@ describe( 'Generate backend seed', function ( ) {
         expect( stderr ).to.equal( '' );
         expect( stdout ).to.not.match( /already exists within/ );
 
-        expect( fs.existsSync( path.join( assetPath, 'my-new-project', 'backend', 'modules', 'Testing2', 'config' ) ) ).to.be.false;
-        expect( fs.existsSync( path.join( assetPath, 'my-new-project', 'backend', 'modules', 'Testing2', 'controllers' ) ) ).to.be.false;
-        expect( fs.existsSync( path.join( assetPath, 'my-new-project', 'backend', 'modules', 'Testing2', 'models' ) ) ).to.be.false;
-        expect( fs.existsSync( path.join( assetPath, 'my-new-project', 'backend', 'modules', 'Testing2', 'schema' ) ) ).to.be.false;
+        expect( fs.readdirSync( path.join( assetPath, 'my-new-project', 'backend', 'modules', 'Testing2') ).length ).to.equal( 1 );
         expect( fs.existsSync( path.join( assetPath, 'my-new-project', 'backend', 'modules', 'Testing2', 'services', 'Testing2Service.js' ) ) ).to.be.true;
-        expect( fs.existsSync( path.join( assetPath, 'my-new-project', 'backend', 'modules', 'Testing2', 'tasks' ) ) ).to.be.false;
-        expect( fs.existsSync( path.join( assetPath, 'my-new-project', 'backend', 'modules', 'Testing2', 'tests' ) ) ).to.be.false;
 
         var service = fs.readFileSync( path.join( assetPath, 'my-new-project', 'backend', 'modules', 'Testing2', 'services', 'Testing2Service.js' ) );
         expect( service ).to.match( /Testing2Service = BaseService.extend\(\{/ );
@@ -147,13 +132,8 @@ describe( 'Generate backend seed', function ( ) {
         expect( stderr ).to.equal( '' );
         expect( stdout ).to.not.match( /already exists within/ );
 
-        expect( fs.existsSync( path.join( assetPath, 'my-new-project', 'backend', 'modules', 'Testing2', 'config' ) ) ).to.be.false;
-        expect( fs.existsSync( path.join( assetPath, 'my-new-project', 'backend', 'modules', 'Testing2', 'controllers' ) ) ).to.be.false;
-        expect( fs.existsSync( path.join( assetPath, 'my-new-project', 'backend', 'modules', 'Testing2', 'models' ) ) ).to.be.false;
-        expect( fs.existsSync( path.join( assetPath, 'my-new-project', 'backend', 'modules', 'Testing2', 'schema' ) ) ).to.be.false;
-        expect( fs.existsSync( path.join( assetPath, 'my-new-project', 'backend', 'modules', 'Testing2', 'services' ) ) ).to.be.false;
+        expect( fs.readdirSync( path.join( assetPath, 'my-new-project', 'backend', 'modules', 'Testing2') ).length ).to.equal( 1 );
         expect( fs.existsSync( path.join( assetPath, 'my-new-project', 'backend', 'modules', 'Testing2', 'tasks', 'Testing2Task.js' ) ) ).to.be.true;
-        expect( fs.existsSync( path.join( assetPath, 'my-new-project', 'backend', 'modules', 'Testing2', 'tests' ) ) ).to.be.false;
 
         var service = fs.readFileSync( path.join( assetPath, 'my-new-project', 'backend', 'modules', 'Testing2', 'tasks', 'Testing2Task.js' ) );
         expect( service ).to.match( /var Testing2Task = module\.exports = Class\.extend\(/ );
@@ -185,12 +165,7 @@ describe( 'Generate backend seed', function ( ) {
         expect( stderr ).to.equal( '' );
         expect( stdout ).to.not.match( /already exists within/ );
 
-        expect( fs.existsSync( path.join( assetPath, 'my-new-project', 'backend', 'modules', 'Testing2', 'config' ) ) ).to.be.false;
-        expect( fs.existsSync( path.join( assetPath, 'my-new-project', 'backend', 'modules', 'Testing2', 'controllers' ) ) ).to.be.false;
-        expect( fs.existsSync( path.join( assetPath, 'my-new-project', 'backend', 'modules', 'Testing2', 'models' ) ) ).to.be.false;
-        expect( fs.existsSync( path.join( assetPath, 'my-new-project', 'backend', 'modules', 'Testing2', 'schema' ) ) ).to.be.false;
-        expect( fs.existsSync( path.join( assetPath, 'my-new-project', 'backend', 'modules', 'Testing2', 'services' ) ) ).to.be.false;
-        expect( fs.existsSync( path.join( assetPath, 'my-new-project', 'backend', 'modules', 'Testing2', 'tasks' ) ) ).to.be.false;
+        expect( fs.readdirSync( path.join( assetPath, 'my-new-project', 'backend', 'modules', 'Testing2') ).length ).to.equal( 1 );
         expect( fs.existsSync( path.join( assetPath, 'my-new-project', 'backend', 'modules', 'Testing2', 'tests', 'integration', 'Testing2Test.js' ) ) ).to.be.true;
         expect( fs.existsSync( path.join( assetPath, 'my-new-project', 'backend', 'modules', 'Testing2', 'tests', 'unit', 'Testing2Test.js' ) ) ).to.be.true;
 
