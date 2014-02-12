cd4f8f9e70fa595fd9ea3e5c10d226c8b2473306
Daniel Durante <me@danieldurante.com>
2014-02-12 16:44:02 -0500

    chore(clean): Attempting to clean up for code climate.

--- tests/generate/frontend/views.test.js
+++ tests/generate/frontend/views.test.js
@@ -9,7 +9,7 @@ var chai      = require( 'chai' )
 
 chai.Assertion.includeStack = true;
 
-describe( 'Generate view files for the frontend seed', function ( ) {
+describe( 'Views', function ( ) {
   after( function ( done ) {
     rimraf( path.join( assetPath, 'my-new-project', 'frontend', 'app', 'modules', 'Testing2' ), done );
   } );
