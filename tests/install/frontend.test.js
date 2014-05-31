d9d43020245315bfd5e194d63057bdeebad974a4
Richard Gustin <richard.gustin86@gmail.com>
2014-05-31 20:49:17 +1000

    Fixing up the REPL

--- tests/install/frontend.test.js
+++ tests/install/frontend.test.js
@@ -22,7 +22,6 @@ describe( 'Install with a frontend module', function ( ) {
   it( 'within the root directory', function ( done ) {
     exec( path.join( binPath, 'clever-install' ) + ' clever-datatables', { cwd: path.join( assetPath, 'my-new-project' ) }, function ( err, stdout, stderr ) {
       expect( stderr ).to.equal( '' );
-      console.log(path.join( assetPath, 'my-new-project', 'frontend', 'app', 'modules', 'cs_datatables', 'bower.json' ))
       expect( fs.existsSync( path.join( assetPath, 'my-new-project', 'frontend', 'app', 'modules', 'cs_datatables', 'bower.json' ) ) ).to.be.true;
 
       var pkg = require( path.join( assetPath, 'my-new-project', 'frontend', 'app', 'modules', 'cs_datatables', 'bower.json' ) );
