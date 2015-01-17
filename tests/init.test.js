e0650d5fa78ee70bdebf4069fbcf33a69b19121c
Richard Gustin <richard@rgnconsulting.com.au>
2015-01-17 19:07:56 +1000

    chore(cleanup): Cleaned up the init tests

--- tests/init.test.js
+++ tests/init.test.js
@@ -1,125 +1,121 @@
-var chai      = require( 'chai' )
-  , expect    = chai.expect
-  , exec      = require('child_process').exec
-  , path      = require( 'path' )
+var path      = require( 'path' )
   , rimraf    = require( 'rimraf' )
   , async     = require( 'async' )
   , fs        = require( 'fs' )
+  , exec      = require('child_process').exec
+  , chai      = require( 'chai' )
+  , expect    = chai.expect
   , binPath   = path.join( __dirname, '..', 'bin' )
   , assetPath = path.join( __dirname, 'assets' );
 
-function tap ( options, done ) {
-  var modules = [ ].concat( options.args || [ ], options.backendModules || [ ], options.frontendModules || [ ] )
-    , command = path.join( binPath, 'clever-init' ) + ' -f -S ' + options.name + (modules.length > 0 ? ' ' + modules.join( ' ' ) : '' );
+function tap( options, done ) {
+    var modules = [ ].concat( options.args || [ ], options.backendModules || [ ], options.frontendModules || [ ] )
+      , command = path.join( binPath, 'clever-init' ) + ' -f -S ' + options.name + ( modules.length > 0 ? ' ' + modules.join( ' ' ) : '' );
 
-  exec( command, { cwd: assetPath }, function ( err, stdout, stderr ) {
-    expect( stderr ).to.equal( '' );
+    exec( command, { cwd: assetPath }, function( err, stdout, stderr ) {
+        expect( stderr ).to.equal( '' );
 
-    if (options.backend === true) {
-      var backendPath = ( options.frontend === true )
-            ? path.join( assetPath, options.name, 'backend' )
-            : path.join( assetPath, options.name );
+        if ( options.backend === true ) {
+            var backendPath = ( options.frontend === true ) ? path.join( assetPath, options.name, 'backend' ) : path.join( assetPath, options.name );
+            expect( fs.existsSync( backendPath ) ).to.be.true;
 
-      expect( fs.existsSync( backendPath ) ).to.be.true;
-      var pkgPath = path.join( backendPath, 'package.json' )
-        , pkgJson = require( pkgPath );
+            var pkgPath = path.join( backendPath, 'package.json' )
+              , pkgJson = require( pkgPath );
 
-      expect( fs.existsSync( pkgPath ) ).to.be.true;
-      expect( pkgJson.name ).to.equal( 'node-seed' );
-    } else {
-      expect( fs.existsSync( path.join( assetPath, options.name, 'backend' ) ) ).to.be.false;
-    }
+            expect( fs.existsSync( pkgPath ) ).to.be.true;
+            expect( pkgJson.name ).to.equal( 'node-seed' );
+        } else {
+            expect( fs.existsSync( path.join( assetPath, options.name, 'backend' ) ) ).to.be.false;
+        }
 
-    if (options.frontend === true) {
-      var frontendPath = ( options.backend === true )
-            ? path.join( assetPath, options.name, 'frontend' )
-            : path.join( assetPath, options.name );
+        if ( options.frontend === true ) {
+            var frontendPath = ( options.backend === true ) ? path.join( assetPath, options.name, 'frontend' ) : path.join( assetPath, options.name );
+            expect( fs.existsSync( frontendPath ) ).to.be.true;
 
-      expect( fs.existsSync( frontendPath ) ).to.be.true;
-      var pkgPath2  = path.join( frontendPath, 'package.json' )
-        , bowerPath = path.join( frontendPath, 'bower.json' );
+            var pkgPath2  = path.join( frontendPath, 'package.json' )
+              , bowerPath = path.join( frontendPath, 'bower.json' );
 
-      expect( fs.existsSync( pkgPath2 ) ).to.be.true;
-      expect( fs.existsSync( bowerPath ) ).to.be.true;
+            expect( fs.existsSync( pkgPath2 ) ).to.be.true;
+            expect( fs.existsSync( bowerPath ) ).to.be.true;
 
-      var pkgJson2  = require( pkgPath2 );
-      expect( pkgJson2.name ).to.equal( 'angular-seed' );
-    } else {
-      expect( fs.existsSync( path.join( assetPath, options.name, 'frontend' ) ) ).to.be.false;
-    }
+            var pkgJson2  = require( pkgPath2 );
+            expect( pkgJson2.name ).to.equal( 'angular-seed' );
+        } else {
+            expect( fs.existsSync( path.join( assetPath, options.name, 'frontend' ) ) ).to.be.false;
+        }
 
-    done( err );
-  } );
+        done( err );
+    });
 }
 
 describe( 'Init (these tests will take a long time)', function ( ) {
-  after( function ( done ) {
-    fs.readdir( assetPath, function ( err, dirs ) {
-      async.each( dirs, function ( dir, next ) {
-        // always keep .gitkeep and we'll use my-new-project within other tests
-        // to help reduce total test time and rebuilding projects
-        if ([ '.gitkeep', 'my-new-project' ].indexOf( dir ) > -1) {
-          return next( );
-        }
+    after( function( done ) {
+        fs.readdir( assetPath, function( err, dirs ) {
+            async.each( dirs, function( dir, next ) {
+                // always keep .gitkeep and we'll use my-new-project within other tests
+                // to help reduce total test time and rebuilding projects
+                if ( [ '.gitkeep', 'my-new-project' ].indexOf( dir ) > -1 ) {
+                    return next( );
+                }
 
-        rimraf( path.join( assetPath, dir ), next );
-      }, done );
-    } );
-  } );
+                rimraf( path.join( assetPath, dir ), next );
+            }, done );
+        });
+    });
 
-  describe( 'should be able to initialize a new project with both backend and frontend', function ( ) {
-    it( 'with no other modules', function ( done ) {
-      tap( {
-        name: 'my-new-project',
-        frontend: true,
-        backend: true
-      }, done );
-    } );
+    describe( 'should be able to initialize a new project with both backend and frontend', function ( ) {
+        it( 'with no other modules', function( done ) {
+            tap({
+                name        : 'my-new-project',
+                backend     : true,
+                frontend    : true
+            }, done );
+        });
 
-    it( 'with modules', function ( done ) {
-      tap( {
-        name: 'my-new-project2',
-        backend: true,
-        frontend: true,
-        backendModules: [ 'backend-example-module' ],
-        frontendModules: [ 'clever-datatables' ]
-      }, function ( err ) {
-        expect( fs.existsSync( path.join( assetPath, 'my-new-project2', 'backend', 'modules', 'backend-example-module' ) ) ).to.be.true;
-        expect( fs.existsSync( path.join( assetPath, 'my-new-project2', 'frontend', 'app', 'modules', 'cs_datatables' ) ) ).to.be.true;
+        it( 'with modules', function( done ) {
+            tap({
+                name            : 'my-new-project2',
+                backend         : true,
+                frontend        : true,
+                backendModules  : [ 'backend-example-module' ],
+                frontendModules : [ 'clever-datatables' ]
+            }, function( err ) {
+                expect( fs.existsSync( path.join( assetPath, 'my-new-project2', 'backend', 'modules', 'backend-example-module' ) ) ).to.be.true;
+                expect( fs.existsSync( path.join( assetPath, 'my-new-project2', 'frontend', 'app', 'modules', 'cs_datatables' ) ) ).to.be.true;
 
-        var csData = path.join( assetPath, 'my-new-project2', 'frontend', 'app', 'modules', 'cs_datatables', 'bower.json' );
-        expect( fs.existsSync( csData ) ).to.be.true;
+                var csData = path.join( assetPath, 'my-new-project2', 'frontend', 'app', 'modules', 'cs_datatables', 'bower.json' );
+                expect( fs.existsSync( csData ) ).to.be.true;
 
-        var csJson = require( csData );
-        expect( csJson.name ).to.equal( 'clever-datatables' );
+                var csJson = require( csData );
+                expect( csJson.name ).to.equal( 'clever-datatables' );
 
-        done( err );
-      } );
-    } );
+                done( err );
+            });
+        });
 
-    it( 'backend', function ( done ) {
-      tap( {
-        name: 'my-new-project3',
-        args: [ 'backend' ],
-        frontend: false,
-        backend: true
-      }, done );
-    } );
+        it( 'backend', function( done ) {
+            tap({
+                name        : 'my-new-project3',
+                args        : [ 'backend' ],
+                backend     : true,
+                frontend    : false
+            }, done );
+        });
 
-    it( 'frontend', function ( done ) {
-      tap( {
-        name: 'my-new-project4',
-        args: [ 'frontend' ],
-        frontend: true,
-        backend: false
-      }, done );
-    } );
+        it( 'frontend', function ( done ) {
+            tap( {
+                name        : 'my-new-project4',
+                args        : [ 'frontend' ],
+                backend     : false,
+                frontend    : true
+            }, done );
+        });
 
-    it( 'should not be able to initialize a new project with the same name', function ( done ) {
-      exec( path.join( binPath, 'clever-init' ) + ' --skip-protractor my-new-project', { cwd: assetPath }, function ( err, stdout ) {
-        expect( stdout ).to.match( /folder already exists/ );
-        done( err );
-      } );
-    } );
-  } );
-} );
+        it( 'should not be able to initialize a new project with the same name', function( done ) {
+            exec( path.join( binPath, 'clever-init' ) + ' --skip-protractor my-new-project', { cwd: assetPath }, function( err, stdout ) {
+                expect( stdout ).to.match( /folder already exists/ );
+                done( err );
+            });
+        });
+    });
+});
