9e337c7ab3a837ab56946aa215577dda20297ebb
Richard Gustin <richard@rgnconsulting.com.au>
2015-03-16 23:59:47 +1000

    refactor(tests): Added directive test

---
+++ tests/generate/frontend/directives.js
@@ -0,0 +1,12 @@
+var path      = require( 'path' )
+  , assetPath = path.join( __dirname, '..', '..', 'assets' );
+
+exports.tap   = function ( done ) {
+  done( null, {
+    file: path.join( assetPath, 'my-new-project', 'frontend', 'app', 'modules', 'Testing2', 'directives', 'Testing2Directive.js' ),
+    matches: [
+      /\.module\('Testing2.directives'\)/,
+      /\.directive\('Testing2Directive',\ function\(\)\ {/
+    ]
+  });
+};
