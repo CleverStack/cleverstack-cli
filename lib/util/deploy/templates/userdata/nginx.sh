d91ab84a69456e4c373d2121ac287e842cbd6ece
Richard <richard.gustin86@gmail.com>
2015-04-04 06:13:44 +1000

    chore(cleanup): nginx config

--- lib/util/deploy/templates/userdata/nginx.sh
+++ lib/util/deploy/templates/userdata/nginx.sh
@@ -85,9 +85,9 @@ http {
 
         location / {
 
-            proxy_pass         http://metrorefunds-prod.s3-website-us-east-1.amazonaws.com/;
+            proxy_pass         http://your-bucket.s3-website-us-east-1.amazonaws.com/;
             proxy_redirect     off;
-            proxy_set_header   Host             "metrorefunds-prod.s3-website-us-east-1.amazonaws.com";
+            proxy_set_header   Host             "your-bucket.s3-website-us-east-1.amazonaws.com";
             proxy_set_header   X-Real-IP        $remote_addr;
             proxy_set_header   X-Forwarded-For  $proxy_add_x_forwarded_for;
             proxy_set_header   X-NginX-Proxy    true;
